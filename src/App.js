00403a196a8e291c9374bc2684e10a3afe232c87
Grzegorz Kaczor <grzesiek125@op.pl>
2021-09-11 17:51:09 +0200

    files scructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch} from 'react-router-dom';
+
+import Login from './components/Login/Login';
+import NewAcount from './components/CreateNewAcount/CreateNewAcount';
+import List from './components/List/List';
+import Layout from './components/Layout/Layout';
 
 function App() {
+  let routes = (
+    <Switch>
+      <Route path='/' component={Login}/>
+      <Route path='/create_acount' component={NewAcount}/>
+      <Route path='/main' component={List}/>
+    </Switch>
+  );
   return(
     <>
-      
+      <Layout>
+        {routes}
+      </Layout>
     </>
   )
 
---
+++ src/components/CreateNewAcount/CreateNewAcount.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class CreateNewAcount extends Component {
+    render() {
+        return 
+    }
+}
+
+export default CreateNewAcount;
