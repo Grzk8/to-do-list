3bf53d5606b367e6f77e2fe68baeadd803c7b9aa
Grzegorz Kaczor <grzesiek125@op.pl>
2021-09-15 11:25:57 +0200

    build

---
+++ docs/precache-manifest.eacfda751faa7adaa01e0be8bc4e3b1a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0d7da75518a13f10db8b3431315324",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "8b43e646d5bc61bd94b3",
+    "url": "/to-do-list/static/css/main.a97c192e.chunk.css"
+  },
+  {
+    "revision": "72f18d47548973ea0607",
+    "url": "/to-do-list/static/js/2.e59d3559.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/to-do-list/static/js/2.e59d3559.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b43e646d5bc61bd94b3",
+    "url": "/to-do-list/static/js/main.ae590bf1.chunk.js"
+  },
+  {
+    "revision": "b955e74af24ad57066da",
+    "url": "/to-do-list/static/js/runtime-main.c51ce417.js"
+  },
+  {
+    "revision": "869732c7745a5189231d234d9dc24ffe",
+    "url": "/to-do-list/static/media/roboto-regular-webfont.869732c7.woff2"
+  },
+  {
+    "revision": "dad6cb29a3b66299c40f84ff5c546083",
+    "url": "/to-do-list/static/media/roboto-regular-webfont.dad6cb29.woff"
+  },
+  {
+    "revision": "3acdb4a9cb4c4aafd7efa5a323ebff1b",
+    "url": "/to-do-list/static/media/zcoolkuaile-regular-webfont.3acdb4a9.woff"
+  },
+  {
+    "revision": "72dfe70eddb32963cd938f69de10aaf4",
+    "url": "/to-do-list/static/media/zcoolkuaile-regular-webfont.72dfe70e.woff2"
+  }
+]);
